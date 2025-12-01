d45d0559e90b26f13caaad39612eb65ced33b01e
Antonin <antonin.lemesle@gmail.com>
2025-12-01 23:57:09 +0100

    Grid done

---
+++ src/Grid.cpp
@@ -0,0 +1,46 @@
+#include <Grid.hpp>
+#include <string>
+#include <Dead.hpp>
+#include <Alive.hpp>
+using namespace std;
+
+Grid::Grid(int l, int c) : lines(l), columns(c) {}
+
+void Grid::init(File* f) {
+    string textfile = f->read();
+    int count = 0;
+    for (char c : textfile) {
+        if (c == '/n') {
+            vector<Cell*> v; 
+            cells.push_back(v);
+            count += 1;
+        }
+        if (c == '0') {
+            Cell* tempcell;
+            CellState* s;
+            s = new Dead(tempcell);
+            tempcell = new Cell(s);
+            cells.at(count).push_back(tempcell);
+        } else if (c == '1' ) {
+            Cell* tempcell;
+            CellState* s;
+            s = new Alive(tempcell);
+            tempcell = new Cell(s);
+            cells.at(count).push_back(tempcell);
+        }
+    }
+}
+
+Grid* Grid::update() {
+    Grid* newGrid;
+    int count = 0;
+    for (vector<Cell*> v : cells) {
+        vector<Cell*> newV;
+        newGrid->cells.push_back(newV);
+        for (Cell* c : v) {
+            newGrid->cells.at(count).push_back(c->evolution());
+        }
+        count += 1;
+    }
+
+}
