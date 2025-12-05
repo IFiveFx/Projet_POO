cd56f905d81ab4f491b984e0320188d3499f86da
gabriel Collin <gabicollin91910@gmail.com>
2025-12-05 13:44:12 +0100

    add cmd argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Game.hpp"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
+    if (argc == 1) argv[1] = "-g";
     Game jeu(2,3,0,argv[1]);
     jeu.run();
     return 0;
